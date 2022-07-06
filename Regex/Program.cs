f558e1b1d89830546f6e33369e7099daff25e9de
nhatminhit <31946502+nhatminhit@users.noreply.github.com>
2022-07-06 14:41:30 +0700

    update regex

---
+++ Regex/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RegexValidate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regexvalidation regex = new Regexvalidation();
+            bool valiPhoneNumber = regex.ValidateMobile();
+            Console.WriteLine("Phone Number"+ " " +valiPhoneNumber);
+            bool valiEmail = regex.ValidateEmail();
+            Console.WriteLine("Email" + " " + valiEmail);
+            bool valiPass = regex.ValidatePass();
+            Console.WriteLine("Password" + " " + valiPass);
+        }
+    }
+}
---
+++ Regex/Regexvalidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexValidate
+{
+    public class Regexvalidation
+    {
+        public static string Phone_Regex = "^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$";
+        //public static string Email_Regex = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([a-zA-Z]{2,3}){0,1}";
+        public static string Email_Regex = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+        public static string Password_Regex = @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]+";
+        public bool ValidateMobile ()
+        {
+            return Regex.IsMatch("(123) 456 7890", Phone_Regex);
+        }
+        public bool ValidateEmail ()
+        {
+            return Regex.IsMatch("asasasa@emailao.cyou", Email_Regex);
+        }
+        public bool ValidatePass()
+        {
+            return Regex.IsMatch("NDXngC$?7])5Wwf{A2a>G^yx}9SYV~%P!@,+pJm4T:[", Password_Regex);
+        }
+    }
+}
