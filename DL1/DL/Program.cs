8552a6f1925f6212237423e3236e28372707af99
nhatminhit <31946502+nhatminhit@users.noreply.github.com>
2022-07-07 09:54:51 +0700

    DL

---
+++ DL1/DL/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+using VisualDump;
+using SQLDumper;
+namespace DL
+{
+    class Program
+    {
+		public class ServiceConsumer
+		{
+			HelloService _hello;
+			public ServiceConsumer(HelloService hello)
+			{
+				_hello = hello;
+			}
+
+			public void Print()
+			{
+				_hello.Print();
+			}
+		}
+		public class HelloService
+		{
+			public void Print()
+			{
+				"Hello World".Dump();
+			}
+		}
+		static void Main(string[] args)
+        {
+			//	var service = new HelloService();
+			//	var consumer = new ServiceConsumer(service);
+			var a = typeof(HelloService).Dump();
+			var service = (HelloService)Activator.CreateInstance(typeof(HelloService));
+			var consumer = (ServiceConsumer)Activator.CreateInstance(typeof(ServiceConsumer), service);
+			service.Print();
+			consumer.Print();
+		}
+    }
+}
