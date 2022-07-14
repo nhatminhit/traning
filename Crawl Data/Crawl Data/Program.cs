a2aaeab213bce05b8c47c6c92205b3726dfa84b7
nhatminhit <31946502+nhatminhit@users.noreply.github.com>
2022-07-14 16:02:33 +0700

    update crawl

---
+++ Crawl Data/Crawl Data/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+namespace Crawl_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * lay du lieu tu trang web 
+             * get static data */
+
+            //var html = new HtmlWeb();
+            //var document = html.Load("https://vnexpress.net/ong-phan-van-mai-tp-hcm-khong-co-nhan-luc-nhan-roi-4486320.html");
+            //var node = document.DocumentNode.SelectSingleNode("html/body/section[4]/div/div[2]/article/p[1]/em/a");
+
+            /* lay du lieu tu chuoi html
+             * get data form html string */
+
+            /* get data from file HTML */
+            var document = new HtmlDocument();
+            document.Load(@"D:\Learn\CTY\Buoi3\Crawl Data\Crawl Data\html.txt");
+            var node = document.DocumentNode.SelectSingleNode("html/body/h1");
+
+            /* Inner Text chỉ get thong tin noi dung bang Text
+             * Inner Html chỉ get thong tin ve html va cac the tag*/
+
+            /* lay gia tri cua thuoc tinh href trong node
+             * get value of href attribute from node*/
+            //Console.WriteLine(node.Attributes["href"].Value);
+            Console.WriteLine(node.InnerText);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Crawl TGDD/Crawl TGDD/Program.cs
@@ -0,0 +1,67 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScrapySharp.Extensions;
+using System.Web;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Crawl_TGDD
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb()
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.UTF8  //Set UTF8 để hiển thị tiếng Việt
+            };
+            //Load trang web, nạp html vào document
+            HtmlDocument document = htmlWeb.Load("https://www.thegioididong.com/dtdd/");
+            //list product
+            var products = document.DocumentNode.CssSelect("ul.listproduct li a[data-site='1']");// bat dau boc tach
+            List<ClassProduct> Listproduct = new List<ClassProduct>();
+            foreach (var product in products)
+            {
+                var productObject = new ClassProduct();//khoi tao object product
+
+                var name = product.CssSelect("h3").FirstOrDefault().InnerText;//gia tri name
+                productObject.name = name;
+                var price = product.CssSelect("strong.price").FirstOrDefault().InnerText;// gia tri price
+                productObject.price = HttpUtility.HtmlEncode(price);
+                var memory = product.CssSelect("ul li");//gia tri memory
+                foreach (var item in memory)
+                {
+                    productObject.memorys.Add(item.InnerText);
+                    Console.WriteLine(item.InnerText);
+                }
+                List<string> imgs = new List<string>();
+                var image = product.CssSelect("img");
+                foreach (var item in image)
+                {
+                    Console.WriteLine(item.GetAttributeValue("src"));
+                    imgs.Add(item.GetAttributeValue("src"));
+                }
+                productObject.images=imgs;
+                Listproduct.Add(productObject);
+            }
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(Listproduct);
+            Console.WriteLine(json);
+            
+        }
+        class ClassProduct
+        {
+            public string name { get; set; }
+            public string  price { get; set; }
+            public List<string> memorys { get; set; } = new List<string>();
+            public List<string> images { get; set; }
+        }
+
+    }
+}
